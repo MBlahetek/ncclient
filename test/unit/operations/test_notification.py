4610ef0e65cf00521e07e161857b7d686493bf81
Alvin Heng <alheng@cisco.com>
2016-09-07 13:52:52 -0700

    3.4 fixed

--- test/unit/operations/test_notification.py
+++ test/unit/operations/test_notification.py
@@ -1,9 +1,11 @@
 import unittest
+
 from ncclient import manager
+from ncclient.operations.util import *
 from ncclient.operations.subscribe import Notification, NotificationType
+
 from lxml import etree
 from dateutil.parser import parse
-from ncclient.operations.util import *
 
 NETCONF_CONFIG_CHANGE = """
 <notification xmlns="urn:ietf:params:xml:ns:netconf:notification:1.0">
@@ -107,7 +109,7 @@ class TestNotification(unittest.TestCase):
     def test_Notification_xml(self):
         xml = [NETCONF_CONFIG_CHANGE, NETCONF_SESSION_END, REPLAY_COMPLETE, NOTIFICATION_COMPLETE]
         for (actual, expected) in zip(self.notifications, xml):
-            self.assertEquals(normalize_xml(actual.xml), normalize_xml(expected))
+            self.assertEqual(normalize_xml(actual.xml), normalize_xml(expected))
 
     def test_Notification_eventtime(self):
         datetimes = [parse('2016-08-02T10:40:40.014291-07:00').isoformat(),
@@ -115,35 +117,35 @@ class TestNotification(unittest.TestCase):
         parse('2016-07-29T14:28:25.793194-07:00').isoformat(),
         parse('2016-07-29T14:28:25.793421-07:00').isoformat()]
         for (actual, expected) in zip(self.notifications, datetimes):
-            self.assertEquals(actual.eventTime.isoformat(), expected)
+            self.assertEqual(actual.eventTime.isoformat(), expected)
 
     def test_Notification_type(self):
         types = [NotificationType.NETCONF_CONFIG_CHANGE, NotificationType.NETCONF_SESSION_END,
         NotificationType.REPLAY_COMPLETE, NotificationType.NOTIFICATION_COMPLETE]
         for (actual, expected) in zip(self.notifications, types):
-            self.assertEquals(actual.type, expected)
+            self.assertEqual(actual.type, expected)
 
     def test_Notification_data_ele(self):
         data_ele = [to_ele(NETCONF_CONFIG_CHANGE_DATA), to_ele(NETCONF_SESSION_END_DATA),
         to_ele(REPLAY_COMPLETE_DATA), to_ele(NOTIFICATION_COMPLETE_DATA)]
         for (actual, expected) in zip(self.notifications, data_ele):
-            self.assertEquals(normalize_xml(actual.data_ele), normalize_xml(expected))
+            self.assertEqual(normalize_xml(actual.data_ele), normalize_xml(expected))
 
     def test_Notification_data_xml(self):
         data_xml = [NETCONF_CONFIG_CHANGE_DATA, NETCONF_SESSION_END_DATA,
         REPLAY_COMPLETE_DATA, NOTIFICATION_COMPLETE_DATA]
         for (actual, expected) in zip(self.notifications, data_xml):
-            self.assertEquals(normalize_xml(actual.data_xml), normalize_xml(expected))
+            self.assertEqual(normalize_xml(actual.data_xml), normalize_xml(expected))
 
     def test_Notification_connected(self):
         connected = [True, False, True, True]
         for (actual, expected) in zip(self.notifications, connected):
-            self.assertEquals(actual.connected, expected)
+            self.assertEqual(actual.connected, expected)
 
     def test_Notification_invalid(self):
         invalid = [False, False, False, False, True]
         for (actual, expected) in zip(self.valid, invalid):
-            self.assertEquals(actual.invalid, expected)
+            self.assertEqual(actual.invalid, expected)
 
 if __name__ == '__main__':
     unittest.main()
